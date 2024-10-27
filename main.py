54985d147bf45ade8aa0cbdb3c2f055abef4a941
blue-hexagon <w0j8uhv5csio@opayq.net>
2024-10-27 09:27:59 +0100

    Fix systemd service file by using full path to poetry

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import platform
+
 from src.deployment.controller.resource_controller import DeploymentController
 from src.main.controller.executor import Executor
 
 if __name__ == '__main__':
-    Executor.run_scraper()
-    # DeploymentController()
+    if platform.system() == "Windows":
+        DeploymentController()
+    else:
+        Executor.run_scraper()
